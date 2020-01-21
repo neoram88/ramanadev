4e521ab77129658f076065ad3e57fae9beecb5df
Venkat <neovenkat88@gmail.com>
2020-01-22 01:47:48 +0530

    set up logo animation bar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,74 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import { useStaticQuery, graphql } from "gatsby"
 import Layout from "../components/layout"
-import Image from "../components/image"
+import Img from "gatsby-image"
 import SEO from "../components/seo"
 import Welcome from "../components/welcome"
+import  "./index.css"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <Welcome/>
-  </Layout>
-)
+const IndexPage = () => {
+
+  // const pngImages = useStaticQuery(graphql`
+  //   query {
+  //     allImageSharp {
+  //       edges {
+  //         node {
+  //           id
+  //           fluid(maxWidth: 200, maxHeight: 200) {
+  //               ...GatsbyImageSharpFluid
+  //           }
+  //         }
+  //         publicURL
+  //         extension
+  //       }
+  //     }
+  //   }
+  // `)
+
+  const data = useStaticQuery(graphql`
+    query{
+      allFile(filter: {extension: {regex: "/(svg)|(png)/"}, relativeDirectory: {eq: "jstools"}}) {
+        edges {
+          node {
+            id
+            name
+            publicURL
+            extension
+            childImageSharp{
+              fixed(width: 30) {
+                  ...GatsbyImageSharpFixed
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <Welcome/>
+      <div className="logoContainer">
+        {data.allFile.edges.map(edge => 
+        {
+          if (edge.node.extension === 'svg') {
+            return <img src={edge.node.publicURL} />
+          }
+          return <Img fixed={edge.node.childImageSharp.fixed} />
+        }
+        )}
+        {data.allFile.edges.map(edge => 
+        {
+          if (edge.node.extension === 'svg') {
+            return <img src={edge.node.publicURL} />
+          }
+          return <Img fixed={edge.node.childImageSharp.fixed} />
+        }
+        )}
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
