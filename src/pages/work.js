17691c7f7adbf496f7a606fa88d6159fcef54532
Venkat <neovenkat88@gmail.com>
2020-01-28 01:06:28 +0530

    Work page changes

--- src/pages/work.js
+++ src/pages/work.js
@@ -13,30 +13,39 @@ const WorkPage = () => {
         <header style={{color: `#42A5F5`}}>
             <h2 style={{ margin: `0 auto`,width:`100px` }}> Work</h2>
         </header>
-        <div className="work-content">
-            <div class="flex">
-                <div class="flex-initial text-center p-4">
-                    <MobileImg/>
-                </div>
-                <div class="flex-initial text-center p-4">
-                    <span>
-                        4 Apps with Angular and Ionic
-                    </span>
-                    <span>
-                        1 App with ReactJS and IBM's Carbon Design
-                    </span>
-                </div>
-            </div>
-            <div>
-
-            </div>
-        </div>
-        <div className="text-white text-xl">
+        <div className="text-white text-lg m-2">
             I have 7 years of experience as a software developer, all of which has been in IBM.
             I have been working with JavaScript based libraries and frameworks for the past 5 years,
             mainly developing Hybrid Mobile Apps, Web Apps and PWAs.
             I specialize in Cross Platform App development, developing Android, 
             iOS and Web Apps using the same shared code base
+        </div>
+        <div className="work-content">
+            <div class="flex">
+                <div class="flex-auto text-center p-4">
+                    <MobileImg/>
+                </div>
+                <div class="flex-auto text-center p-4">
+                    <div className="text-2xl">
+                        <span className="text-secondary" 
+                        style={{fontFamily: `Montserrat SemiBold`}}> 4 </span>
+                        <span className="text-light m-auto">
+                            Apps with <span className="text-red"> Angular </span>
+                            and <span className="text-ionicblue">Ionic</span>
+                        </span>
+                    </div>
+                    <div className="text-2xl">
+                        <span className="text-secondary"> 1 </span>
+                        <span className="text-light m-auto">
+                            Apps with <span className="text-reactblue"> ReactJS </span>
+                            and IBM's Carbon Design
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <div>
+
+            </div>
         </div>
     </Layout>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,12 @@ module.exports = {
     colors: {
       primary: '#42A5F5',
       secondary: '#FFA3D8',
-      red: '#de3618',
+      red: '#DD0031',
       white: '#FFFFFF',
       dark: '#1D1D1D',
-      light: '#DBE1E6'
+      light: '#DBE1E6',
+      ionicblue: '#478AFF',
+      reactblue: '#53C1DE'
     },
     extend: {}
   },
